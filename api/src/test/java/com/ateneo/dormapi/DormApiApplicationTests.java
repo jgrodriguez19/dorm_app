52ed8d46e497a8d30536b678fe30bad10add6434
aasherkataria <aasherkataria@outlook.com>
2022-08-01 21:57:24 -0500

    Init SpringBoot project for API creation - CR Github

---
+++ api/src/test/java/com/ateneo/dormapi/DormApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ateneo.dormapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DormApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
